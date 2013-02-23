31f9d94d98ff7a36a8b61ca905180a6b32af0717
ezraball <ezraball@gmail.com>
2013-02-23 00:05:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmascard::Application.config.secret_token = '71510a3846b18515fe42f90c3b3380c07de855d52d162d062622db403bc7a1c3a323aa56312ff1cf2d4af86302b306eecb850dc6babd4430bdfb3516d6d8738c'
